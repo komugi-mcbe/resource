d9eff71a197a0370b315e53c9b3fc80fdec6ec9b
たくまっち   Takumachi <47268002+xtakumatutix@users.noreply.github.com>
2020-05-26 05:17:53 +0900

    ちょっとミス

--- src/xtakumatutix/resource/EventListener.php
+++ src/xtakumatutix/resource/EventListener.php
@@ -7,7 +7,6 @@ use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\math\Vector3;
 use pocketmine\block\Block;
 use pocketmine\level\Level;
-use pocketmine\level\Position;
 use pocketmine\plugin\Plugin;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\Server;
@@ -34,30 +33,28 @@ class EventListener implements Listener
         $z = $block->getZ();
         $level = $player->getLevel()->getName();
         if ($level == "resource")
-            if (!$player->isOP()) {
-                {
-                    switch ($blockid) {
-                        case 87:
-                        case 49:
-                        case 121:
-                        case 155:
-                        case 45:
-                        case 98:
-                        case 12:
-                        case 3:
-                        case 17:
-                        case 1:
-                            $this->set($blockid, $blockdamage, $x, $y, $z);
+            switch ($blockid) {
+                case 87:
+                case 49:
+                case 121:
+                case 155:
+                case 45:
+                case 98:
+                case 12:
+                case 3:
+                case 17:
+                case 1:
+                    $this->set($blockid, $blockdamage, $x, $y, $z);
 
-                            if (mt_rand(1, 10) === 2) {
-                                $money = mt_rand(15, 220);
-                                EconomyAPI::getInstance()->addMoney($name, $money);
-                                $player->sendPopup("§aGet!! §f" . $money . "§6K§eG");
-                            }
+                    if (mt_rand(1, 10) === 2) {
+                        $money = mt_rand(15, 220);
+                        EconomyAPI::getInstance()->addMoney($name, $money);
+                        $player->sendPopup("§aGet!! §f" . $money . "§6K§eG");
                     }
-                }
             }
     }
+
+
 
 
     public function set($blockid, $blockdamage, $x, $y, $z)
